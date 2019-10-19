37ec9afde6c243f32033adac98fd16d9596ebf5f
benjenkinsv95 <benjenkinsv95@gmail.com>
2019-10-19 17:22:04 -0400

    Updates

---
+++ precache-manifest.5a560b091529073e31b8c24fc8f7bc0c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2b842f9f49b29a933e8a0fd6a31e021",
+    "url": "/penguins-agram/index.html"
+  },
+  {
+    "revision": "fa888c550830096978c4",
+    "url": "/penguins-agram/static/css/main.dfca195d.chunk.css"
+  },
+  {
+    "revision": "db4f3e403cc0d3017496",
+    "url": "/penguins-agram/static/js/2.f3bd4a70.chunk.js"
+  },
+  {
+    "revision": "fa888c550830096978c4",
+    "url": "/penguins-agram/static/js/main.579eac79.chunk.js"
+  },
+  {
+    "revision": "8a553fe1f43f0d0e7cc7",
+    "url": "/penguins-agram/static/js/runtime-main.d3fd289f.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/penguins-agram/static/media/logo.25bf045c.svg"
+  }
+]);
